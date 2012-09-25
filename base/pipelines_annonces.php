7f531be709bacc5e057d3cbfe3f979f39af4ea83
TheoChevalier <theo.chevalier11@gmail.com>
2012-09-25 11:31:47 +0200

    Pipelines_annonces.php

---
+++ base/pipelines_annonces.php
@@ -0,0 +1,48 @@
+<?php
+ function prefixplugin_init_tables_principales($tables_principales){
+        $new_inscription = array(
+                "email" => "VARCHAR(100) NOT NULL",
+                "nom" => "VARCHAR(100) NOT NULL",
+                "mdp" => "VARCHAR(32) NOT NULL",
+                "key" => "VARCHAR(16) NOT NULL",
+                "confirm" => "BOOLEAN NOT NULL"
+        );
+
+        $new_inscription_cles = array(
+                "PRIMARY KEY" => "email"
+        );
+        
+        $new_inscription_join = array(
+                "email" => "email"
+        );
+        
+        $new_moderation = array(
+                "id_annonce" => "INT(11) NOT NULL AUTO_INCREMENT"
+                "titre" => "VARCHAR(300) NOT NULL",
+                "contenu" => "VARCHAR(100) NOT NULL",
+                "tel" => "VARCHAR(10)",
+                "prix" => "INT(11)",
+        );
+
+        $new_inscription_cles = array(
+                "PRIMARY KEY" => "email"
+        );
+        
+        $new_inscription_join = array(
+                "email" => "email"
+        );
+
+        $tables_principales['spip_inscription'] = array(
+                'field' => &$new_inscription,
+                'key' => &$new_inscription_cles,
+                'join' => &$new_inscription_join
+        );
+        $tables_principales['spip_moderation'] = array(
+                'field' => &$new_moderation,
+                'key' => &$new_moderation_cles,
+                'join' => &$new_moderation_join
+        );
+        
+        return $tables_principales;
+}
+?>
